148cb30e5d9e67f4a123b8ef7bc98e0b6d8a44c4
Anthony Lipin <tonylipin@gmail.com>
2021-01-21 20:06:07 +0000

    more ferris

--- main.rs
+++ main.rs
@@ -1,23 +1,20 @@
-use rand::Rng;
+#[macro_use]
+extern crate ferris_print;
 extern crate ferris_says;
 use ferris_says::say;
 use std::io::{ stdout, BufWriter };
+use rand::Rng;
 
-	fn main() {
+ fn main() {
     let out = b"Hello fellow Rustaceans!";
     let width = 24;
-
-    let mut writer = BufWriter::new(stdout());
+		let mut writer = BufWriter::new(stdout());
     say(out, width, &mut writer).unwrap();
-   
-
-   
-	  let mut rng = rand::thread_rng();
-
-    let n1: u8 = rng.gen();
+   	let mut rng = rand::thread_rng();
+		let n1: u8 = rng.gen();
     let n2: u16 = rng.gen();
-    println!("Random u8: {}", n1);
-    println!("Random u16: {}", n2);
-    
-}
-
+		#[macro_use]
+    ferrisprint!("Random u8: {}", n1);
+		#[macro_use]
+    ferrisprint!("Random u16: {}", n2);
+  }
