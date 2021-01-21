a43abcf2c0c1ab724a1b92851025f6df3b4b4c08
Anthony Lipin <tonylipin@gmail.com>
2021-01-21 18:15:00 +0000

    now with Ferris the crab

--- main.rs
+++ main.rs
@@ -1,10 +1,23 @@
 use rand::Rng;
- 
+extern crate ferris_says;
+use ferris_says::say;
+use std::io::{ stdout, BufWriter };
 
-fn main(){
-	loop {
-	let mut rng = rand::thread_rng();
-	let r1: u8 = rng.gen();
-	println! ("num {}", r1);
+	fn main() {
+    let out = b"Hello fellow Rustaceans!";
+    let width = 24;
+
+    let mut writer = BufWriter::new(stdout());
+    say(out, width, &mut writer).unwrap();
+   
+
+   
+	  let mut rng = rand::thread_rng();
+
+    let n1: u8 = rng.gen();
+    let n2: u16 = rng.gen();
+    println!("Random u8: {}", n1);
+    println!("Random u16: {}", n2);
+    
 }
-}
+
