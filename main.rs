ced694bce8a992d1c781c38e8f37df19fefbdea6
Anthony Lipin <tonylipin@gmail.com>
2021-01-22 05:00:54 +0000

    yay its done

--- main.rs
+++ main.rs
@@ -4,20 +4,19 @@ extern crate ferris_print;
 use rand::Rng;
 use std::process::Command;
 
-
- fn main() {
-	  let mut child = Command::new("sleep").arg("1").spawn().unwrap();
+fn main() {
+    let mut child = Command::new("sleep").arg("1").spawn().unwrap();
     let _result = child.wait().unwrap();
-		ferrisprint!("Hello fellow Rustaceans!");
+    ferrisprint!("Hello fellow Rustaceans!");
     let mut rng = rand::thread_rng();
-		let n1: u8 = rng.gen();
+    let n1: u8 = rng.gen();
     let n2: u16 = rng.gen();
-		let mut child = Command::new("sleep").arg("3").spawn().unwrap();
+    let mut child = Command::new("sleep").arg("3").spawn().unwrap();
     let _result = child.wait().unwrap();
-		#[macro_use]
+    #[macro_use]
     ferrisprint!("Random u8: {}", n1);
-		let mut child = Command::new("sleep").arg("3").spawn().unwrap();
+    let mut child = Command::new("sleep").arg("3").spawn().unwrap();
     let _result = child.wait().unwrap();
-		#[macro_use]
+    #[macro_use]
     ferrisprint!("Random u16: {}", n2);
-  }
+}
