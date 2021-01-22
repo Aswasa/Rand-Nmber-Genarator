c028ca0922d3ecc11a1c277ca942251c7107d95e
Anthony Lipin <tonylipin@gmail.com>
2021-01-21 23:15:40 -0500

    optomize

--- main.rs
+++ main.rs
@@ -1,16 +1,11 @@
 #[macro_use]
 extern crate ferris_print;
 extern crate ferris_says;
-use ferris_says::say;
-use std::io::{ stdout, BufWriter };
 use rand::Rng;
 
  fn main() {
-    let out = b"Hello fellow Rustaceans!";
-    let width = 24;
-		let mut writer = BufWriter::new(stdout());
-    say(out, width, &mut writer).unwrap();
-   	let mut rng = rand::thread_rng();
+    ferrisprint!("Hello fellow Rustaceans!");
+    let mut rng = rand::thread_rng();
 		let n1: u8 = rng.gen();
     let n2: u16 = rng.gen();
 		#[macro_use]
