2f025cb8ed8d26b67d7e441d0afa54e698c6073c
Anthony Lipin <tonylipin@gmail.com>
2021-01-22 04:31:55 +0000

    added sleep

--- main.rs
+++ main.rs
@@ -1,15 +1,23 @@
+#![allow(unused)]
 #[macro_use]
 extern crate ferris_print;
-extern crate ferris_says;
 use rand::Rng;
+use std::process::Command;
+
 
  fn main() {
-    ferrisprint!("Hello fellow Rustaceans!");
+	  let mut child = Command::new("sleep").arg("1").spawn().unwrap();
+    let _result = child.wait().unwrap();
+		ferrisprint!("Hello fellow Rustaceans!");
     let mut rng = rand::thread_rng();
 		let n1: u8 = rng.gen();
     let n2: u16 = rng.gen();
+		let mut child = Command::new("sleep").arg("3").spawn().unwrap();
+    let _result = child.wait().unwrap();
 		#[macro_use]
     ferrisprint!("Random u8: {}", n1);
+		let mut child = Command::new("sleep").arg("3").spawn().unwrap();
+    let _result = child.wait().unwrap();
 		#[macro_use]
     ferrisprint!("Random u16: {}", n2);
   }
